8af15facf4716632db7034c64d937b5e6c2f8db9
dev5020 <omtfzz@gmail.com>
2018-03-25 23:05:58 +0800

    add routers and views

--- blog/urls.py
+++ blog/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+"""
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+
+
+urlpatterns = [
+    (r'^$', 'blogpost.views.index'),
+    url(r'^blog/(?P<slug>[^\.]+).html', 'blogpost.views.view_post', name='view_blog_post'),
+    url(r'^admin/', include(admin.site.urls))
+]
+
